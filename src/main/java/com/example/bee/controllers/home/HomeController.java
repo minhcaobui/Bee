21ebaa3aefcb702eab6d1913cd195f7d1e277f14
minhcaobui <minhbc.work@gmail.com>
2026-04-09 23:41:28 +0700

    fix home

--- src/main/java/com/example/bee/controllers/home/HomeController.java
+++ src/main/java/com/example/bee/controllers/home/HomeController.java
@@ -8,9 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        // Tự động chuyển hướng khách vào trang đăng nhập khi gõ domain gốc
         return "redirect:/login";
-
-        // (Hoặc nếu bạn có trang chủ cho khách mua hàng, bạn đổi thành: return "redirect:/trang-chu";)
     }
 }
